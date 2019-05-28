2a1c725f44061cd64eb20a59d010815004e1706a
smalls1652 <smalls.online@icloud.com>
2019-05-28 09:21:48 -0400

    Added RequiredModules param.
    - Added RequiredModules parameter to Update-ModuleProject.

--- ScriptFiles/Update-ModuleProject/Update-ModuleProject.ps1
+++ ScriptFiles/Update-ModuleProject/Update-ModuleProject.ps1
@@ -3,6 +3,7 @@ param(
     [string]$ProjectPath,
     [string]$ModuleVersion,
     [string]$Description,
+    [string[]]$RequiredModules,
     [int]$Copyright
 )
 
@@ -13,11 +14,16 @@ begin {
 
     Write-Verbose "Loading ModuleManifest config file..."
     $ModuleManifestData = ConvertFrom-Json -InputObject (Get-Content -Path (Join-Path -Path "Configs" -ChildPath "modulemanifest.json") -Raw)
+
+    if (!($ModuleManifestData.RequiredModules)) {
+        Add-Member -InputObject $ModuleManifestData -MemberType NoteProperty -Name "RequiredModules" -Value @()
+    }
 }
 
 process {
     $ModuleManifestSplat = @{
-        "Path" = $ModuleManifestData.Path
+        "Path" = $ModuleManifestData.Path;
+        "RequiredModules" = $ModuleManifestData.RequiredModules
     }
 
     if ($ModuleVersion) {
@@ -38,6 +44,16 @@ process {
         $ModuleManifestSplat.Add("Copyright", $Copyright)
     }
 
+    if ($RequiredModules) {
+        foreach ($Item in $RequiredModules) {
+            if ($ModuleManifestData.RequiredModules -notcontains $Item) {
+                Write-Verbose "Adding '$($Item)' to RequiredModules."
+                $ModuleManifestData.RequiredModules += $Item
+                $ModuleManifestSplat.RequiredModules += $Item
+            }
+        }
+    }
+
     Write-Verbose "Writing data back to ModuleManifest config file..."
     $ModuleManifestData | ConvertTo-Json -Depth 10 | Out-File -FilePath (Join-Path -Path "Configs" -ChildPath "modulemanifest.json")
 
@@ -45,7 +61,7 @@ process {
     Update-ModuleManifest @ModuleManifestSplat
 }
 
-end{
+end {
     Pop-Location -StackName "Original"
     Write-Verbose "Done."
     return $ModuleManifestData
